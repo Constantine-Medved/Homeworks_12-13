1c821cee6cfbff28368f80da7e6140a3efd892ad
Constantine-Medved <medved.k@outlook.com>
2023-03-18 21:36:28 +0300

    Homework12 completed

---
+++ src/Homework12/Book.java
@@ -0,0 +1,23 @@
+package Homework12;
+public class Book {
+    private String bookTitle;
+    private Author bookAuthor;
+    private int bookYear;
+    public Book (String bookTitle, Author bookAuthor, int bookYear) {
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookYear = bookYear;
+    }
+    public String getBookTitle() {
+        return this.bookTitle;
+    }
+    public Author getBookAuthor () {
+        return this.bookAuthor;
+    }
+    public int getBookYear () {
+        return this.bookYear;
+    }
+    public void setBookYear(int bookYear) {
+        this.bookYear = bookYear;
+    }
+}
--- src/Homework12/Homework12.java
+++
@@ -1,4 +0,0 @@
-package Homework12;
-public class Homework12 {
-    public static void main(String[] args) {System.out.println("Hello world!");}
-    }
