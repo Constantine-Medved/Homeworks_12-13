fb4b43d651bda18719985aabe879b9ab124f9e19
Constantine-Medved <medved.k@outlook.com>
2023-03-18 23:21:21 +0300

    Homework13_initial commit

---
+++ src/Homework12_13/Main.java
@@ -0,0 +1,12 @@
+package Homework12_13;
+public class Main {
+    public static void main(String[] args) {
+        Author authorAlexanderPushkin = new Author("Alexander", "Pushkin");
+        Author authorNikolayGogol = new Author("Nikolay", "Gogol");
+        Book bookMetel = new Book("Metel", authorAlexanderPushkin, 2020);
+        Book bookShinel = new Book("Shinel", authorNikolayGogol, 2017);
+        System.out.println("bookShinel.getBookYear = " + bookShinel.getBookYear());
+        bookShinel.setBookYear(2020);
+        System.out.println("bookShinel.getBookYear = " + bookShinel.getBookYear());
+    }
+}
